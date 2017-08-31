506d6d4506b0708b7aad265bcc6b1343af82c232
Alfredo Lopez <alfredlopezf@hotmail.com>
2017-08-31 14:21:26 -0400

    Updated the script builds to make them one file

--- Code/build.sh
+++ Code/build.sh
@@ -5,12 +5,24 @@ BACK_END=./VIR-Backend
 OUT_PUT_PATH=../VIR-Backend/src/main/resources/static
 DIR=$(pwd)
 
+# Set environment properties
+if [[ $1 == 'prod' ]]
+then
+	echo Building prod
+	ENV_FLAG_MAVEN='-Pprod'
+	ENV_FLAG_NG='--env=prod'
+else
+	echo 'Building dev (default)'
+	ENV_FLAG_MAVEN=
+	ENV_FLAG_NG=
+fi
+
 # Install the angular dependencies and the project
 cd $FRONT_END
 echo Installing dependencies
 npm install
 echo Building the frontend...
-ng build
+ng build -op "$OUT_PUT_PATH" $ENV_FLAG_NG
 cd $DIR
 
 # Clean the maven backend and rebuild
@@ -18,5 +30,6 @@ cd $BACK_END
 echo Cleaning the backend...
 mvn clean
 echo Installing the backend...
-mvn install
+mvn install $ENV_FLAG_MAVEN
 cd $DIR
+	
