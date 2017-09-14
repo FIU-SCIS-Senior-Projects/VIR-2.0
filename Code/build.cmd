26b628b710a13f9c9b779b5af67c74ae5ee1d2f9
Alfredo Lopez <alfredlopezf@hotmail.com>
2017-09-14 19:44:12 -0400

    Added the correct executable from Maven

--- Code/build.cmd
+++ Code/build.cmd
@@ -3,8 +3,8 @@
 :: Set some of the commands
 if "%1"=="prod" (
 	echo Building prod
-	call mvnw.cmd clean install -Pprod -Dnpm.executable=npm.cmd -Dng.executbale=ng.cmd -Dng.production.flag="--env=prod"
+	call mvnw.cmd clean install -Pprod -Dnpm.executable=npm.cmd -Dng.executable=ng.cmd -Dng.production.flag="--env=prod"
 ) else (
 	echo Building dev default
-	call mvnw.cmd clean install -Dnpm.executable=npm.cmd -Dng.executbale=ng.cmd
+	call mvnw.cmd clean install -Dnpm.executable=npm.cmd -Dng.executable=ng.cmd
 )
--- Code/build.sh
+++ Code/build.sh
@@ -4,8 +4,8 @@
 if [[ $1 == 'prod' ]]
 then
 	echo Building prod
-	./mvnw clean install -Pprod -Dnpm.executable=npm -Dng.executbale=ng -Dng.production.flag="--env=prod"
+	./mvnw clean install -Pprod -Dnpm.executable=npm -Dng.executable=ng -Dng.production.flag="--env=prod"
 else
 	echo 'Building dev (default)'
-	./mvnw clean install -Dnpm.executable=npm -Dng.executbale=ng
+	./mvnw clean install -Dnpm.executable=npm -Dng.executable=ng
 fi
