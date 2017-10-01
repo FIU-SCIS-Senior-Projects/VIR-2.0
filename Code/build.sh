f0c193c47de65cd0d5f2364f5354f1893313693d
Alfredo Lopez <alfredlopezf@hotmail.com>
2017-10-01 11:10:46 -0400

    Changed the pipeline to update versions

--- Code/build.sh
+++ Code/build.sh
@@ -3,6 +3,8 @@
 # Set some of the commands
 if [[ $1 == 'prod' ]]
 then
+	echo Increasing build values in prod
+	./mvnw build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} -DprocessAllModules=true -DnextSnapshot=true versions:commit
 	echo Building prod
 	./mvnw clean install -Pprod -Dnpm.executable=npm -Dng.executable=ng -Dng.production.flag="--env=prod"
 else
