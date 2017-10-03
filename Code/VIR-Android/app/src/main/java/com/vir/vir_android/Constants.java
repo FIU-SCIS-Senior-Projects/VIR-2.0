8f4b06a932d1a436cfcebf962e6f772655b68f2d
Alfredo Lopez <alfredlopezf@hotmail.com>
2017-10-03 16:28:06 -0400

    Added the test android project

---
+++ Code/VIR-Android/app/src/main/java/com/vir/vir_android/Constants.java
@@ -0,0 +1,11 @@
+package com.vir.vir_android;
+
+/**
+ * Created by alfredo on 10/2/17.
+ */
+
+public interface Constants {
+    public static final String DOMAIN = "myvirs.com";
+    public static final String DOMAIN_FULL = "http://www.myvirs.com";
+}
+
---
+++ Code/VIR-Android/app/src/main/java/com/vir/vir_android/MainActivity.java
@@ -0,0 +1,37 @@
+package com.vir.vir_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl(Constants.DOMAIN_FULL);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new MyAppWebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
