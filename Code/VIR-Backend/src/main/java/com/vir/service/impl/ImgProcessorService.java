96faf5da31d28deb8d67d5358b2d8d8a7487edc4
Alfredo Lopez <alfredlopezf@hotmail.com>
2017-10-03 19:36:15 -0400

    Added the basic OCR for images

--- Code/VIR-Backend/src/main/java/com/vir/service/impl/ImgProcessorService.java
+++ Code/VIR-Backend/src/main/java/com/vir/service/impl/ImgProcessorService.java
@@ -1,18 +1,48 @@
 package com.vir.service.impl;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.vir.model.FileType;
 import com.vir.model.Text;
 import com.vir.service.FileProcessorService;
+import com.vir.service.TextProcessorService;
+
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
 
 @Service("imgProcessorService")
 public class ImgProcessorService implements FileProcessorService {
 
+	@Autowired
+	@Qualifier("optimizedTextProcessorService")
+	private TextProcessorService textProcessorService;
+
+	@Value("${TESSDATA_PREFIX:./}")
+	String tessData;
+
 	@Override
-	public Text process(MultipartFile file, FileType type) {
-		// TODO Auto-generated method stub
-		return null;
+	public Text process(MultipartFile file, FileType type) throws Exception {
+
+		try {
+			ITesseract instance = new Tesseract();
+			instance.setDatapath(tessData);
+
+			File convFile = File.createTempFile("temp", file.getOriginalFilename());
+			FileOutputStream fos = new FileOutputStream(convFile);
+			fos.write(file.getBytes());
+			fos.close();
+
+			String result = instance.doOCR(convFile);
+			return textProcessorService.process(result.toString());
+		} catch (Exception e) {
+			throw e;
+		}
 	}
 }
