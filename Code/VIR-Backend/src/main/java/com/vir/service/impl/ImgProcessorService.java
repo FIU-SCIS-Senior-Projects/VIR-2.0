655ffedd1368c1b1e8591063910bff1403db7635
Alfredo Lopez <alfredlopezf@hotmail.com>
2017-10-04 21:13:35 -0400

    Added the newwer way to use Tesseract

--- Code/VIR-Backend/src/main/java/com/vir/service/impl/ImgProcessorService.java
+++ Code/VIR-Backend/src/main/java/com/vir/service/impl/ImgProcessorService.java
@@ -1,8 +1,11 @@
 package com.vir.service.impl;
 
-import java.io.File;
-import java.io.FileOutputStream;
-
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.parser.ocr.TesseractOCRConfig;
+import org.apache.tika.sax.BodyContentHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,9 +17,6 @@ import com.vir.model.Text;
 import com.vir.service.FileProcessorService;
 import com.vir.service.TextProcessorService;
 
-import net.sourceforge.tess4j.ITesseract;
-import net.sourceforge.tess4j.Tesseract;
-
 @Service("imgProcessorService")
 public class ImgProcessorService implements FileProcessorService {
 
@@ -24,25 +24,43 @@ public class ImgProcessorService implements FileProcessorService {
 	@Qualifier("optimizedTextProcessorService")
 	private TextProcessorService textProcessorService;
 
-	@Value("${TESSDATA_PREFIX:./}")
-	String tessData;
+	@Value("${TESSDATA_PREFIX}")
+	String tessdataPath;
+
+	@Value("${TESSERACT_PATH}")
+	String tesseractPath;
 
 	@Override
 	public Text process(MultipartFile file, FileType type) throws Exception {
 
-		try {
-			ITesseract instance = new Tesseract();
-			instance.setDatapath(tessData);
+		Parser JpegParser = new AutoDetectParser();
+		BodyContentHandler handler = new BodyContentHandler();
 
-			File convFile = File.createTempFile("temp", file.getOriginalFilename());
-			FileOutputStream fos = new FileOutputStream(convFile);
-			fos.write(file.getBytes());
-			fos.close();
+		TesseractOCRConfig config = new TesseractOCRConfig();
+		config.setTessdataPath(tessdataPath);
+		config.setTesseractPath(tesseractPath);
 
-			String result = instance.doOCR(convFile);
-			return textProcessorService.process(result.toString());
-		} catch (Exception e) {
-			throw e;
-		}
+		
+		ParseContext parseContext = new ParseContext();
+		parseContext.set(TesseractOCRConfig.class, config);
+		
+		JpegParser.parse(file.getInputStream(), handler, new Metadata(), parseContext);
+		return textProcessorService.process(handler.toString());
+
+		//
+		// try {
+		// ITesseract instance = new Tesseract();
+		// instance.setDatapath(tessData);
+		//
+		// File convFile = File.createTempFile("temp", file.getOriginalFilename());
+		// FileOutputStream fos = new FileOutputStream(convFile);
+		// fos.write(file.getBytes());
+		// fos.close();
+		//
+		// String result = instance.doOCR(convFile);
+		// return textProcessorService.process(result.toString());
+		// } catch (Exception e) {
+		// throw e;
+		// }
 	}
 }
