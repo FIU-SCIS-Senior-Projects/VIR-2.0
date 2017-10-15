f0a2d2f0dded31769985ca13225fe7e5a4adc7b3
Alfredo Lopez <alfredlopezf@hotmail.com>
2017-10-15 17:24:06 -0400

    Added the OCR optimization for images that are skewd

--- Code/VIR-Backend/src/main/java/com/vir/service/impl/ImgProcessorService.java
+++ Code/VIR-Backend/src/main/java/com/vir/service/impl/ImgProcessorService.java
@@ -9,11 +9,14 @@ import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
 import org.apache.tika.parser.ocr.TesseractOCRConfig;
 import org.apache.tika.sax.BodyContentHandler;
+import org.opencv.core.Mat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.vir.exception.UnparseableContentException;
+import com.vir.helpers.IOHelper;
 import com.vir.model.FileType;
 import com.vir.model.Text;
 import com.vir.service.FileProcessorService;
@@ -28,10 +31,7 @@ public class ImgProcessorService implements FileProcessorService {
 
 	@Autowired
 	private OcrOptimizerService ocrOptimizerService;
-	
-	@Autowired
-	private OcrOptimizerService ocrOptimizer;
-	
+
 	@Autowired
 	private TesseractConfigurationService tessConfiguration;
 
@@ -40,20 +40,19 @@ public class ImgProcessorService implements FileProcessorService {
 
 		Parser JpegParser = new AutoDetectParser();
 		BodyContentHandler handler = new BodyContentHandler();
-		
 		ParseContext parseContext = new ParseContext();
 		parseContext.set(TesseractOCRConfig.class, tessConfiguration.getConfig());
-
-		InputStream deSkewedInputStream = ocrOptimizerService.deskew(file.getInputStream());
-		InputStream bwInputStream = ocrOptimizerService.toGreyScale(deSkewedInputStream);
-		InputStream biInputStream =  ocrOptimizer.binarize(bwInputStream);
-
-		JpegParser.parse(biInputStream, handler, new Metadata(), parseContext);
+	
+		// Optimize the image for OCR
+		Mat image = IOHelper.inputStreamToMat(file.getInputStream());
+		Mat optimizedImage = ocrOptimizerService.optimize(image);
+		InputStream processedImage = IOHelper.matToInputStream(optimizedImage);
+		
+		JpegParser.parse(processedImage, handler, new Metadata(), parseContext);
 
 		if (StringUtils.isEmpty(handler.toString().trim())) {
-			return new Text();
+			throw new UnparseableContentException("Could not parse the file.");
 		}
-
 		return textProcessorService.process(handler.toString());
 	}
 }
--- Code/VIR-Backend/src/main/java/com/vir/service/impl/OcrOptimizerService.java
+++ Code/VIR-Backend/src/main/java/com/vir/service/impl/OcrOptimizerService.java
@@ -1,62 +1,127 @@
 package com.vir.service.impl;
 
-import static org.opencv.imgcodecs.Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE;
-import static org.opencv.imgproc.Imgproc.GaussianBlur;
+import static org.opencv.imgproc.Imgproc.COLOR_BGR2GRAY;
 import static org.opencv.imgproc.Imgproc.THRESH_BINARY;
 import static org.opencv.imgproc.Imgproc.THRESH_OTSU;
 
-import java.awt.image.BufferedImage;
-import java.io.InputStream;
-
-import javax.imageio.ImageIO;
-
-import org.apache.commons.io.IOUtils;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
+import org.opencv.core.Point;
 import org.opencv.core.Size;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.springframework.stereotype.Service;
 
-import com.recognition.software.jdeskew.ImageDeskew;
-import com.vir.helpers.IOHelper;
-
-import net.sourceforge.tess4j.util.ImageHelper;
 import nu.pattern.OpenCV;
 
 @Service
 public class OcrOptimizerService {
 
-	public InputStream deskew(InputStream fileInputStream) throws Exception {
-
-		BufferedImage bi = ImageIO.read(fileInputStream);
-		ImageDeskew id = new ImageDeskew(bi);
-		double imageSkewAngle = id.getSkewAngle();
-		double MINIMUM_DESKEW_THRESHOLD = 0.05d;
-
-		if ((imageSkewAngle > MINIMUM_DESKEW_THRESHOLD || imageSkewAngle < -(MINIMUM_DESKEW_THRESHOLD))) {
-			bi = ImageHelper.rotateImage(bi, -imageSkewAngle);
-		}
-		
-		return IOHelper.bufferedImageToInputStream(bi);
-	}
-
-	public InputStream toGreyScale(InputStream fileInputStream) throws Exception {
-		BufferedImage bi = ImageIO.read(fileInputStream);
-		BufferedImage bibw = ImageHelper.convertImageToGrayscale(bi);
-		return IOHelper.bufferedImageToInputStream(bibw);
-	}
-
-	public InputStream binarize(InputStream fileInputStream) throws Exception {
+	/**
+	 * Load the library once. We use the constructor and Autowiring to make sure we
+	 * have this in our class path.
+	 */
+	public OcrOptimizerService() {
 		OpenCV.loadShared();
-		byte[] bytes = IOUtils.toByteArray(fileInputStream);
-		Mat gray = Imgcodecs.imdecode(new MatOfByte(bytes), CV_LOAD_IMAGE_GRAYSCALE);
-		Mat gray_with_gauss = new Mat();
-		Mat finalResult = new Mat();
-
-		GaussianBlur(gray, gray_with_gauss, new Size(5, 5), 0);
-		Imgproc.threshold(gray_with_gauss, finalResult, 0, 255, THRESH_BINARY + THRESH_OTSU);
-
-		return IOHelper.Mat2InputStream(finalResult);
 	}
+
+	public Mat optimize(Mat image) {
+		return deskew(image);
+	}
+
+	
+	public Mat deskew(Mat image) {
+
+		Mat result = new Mat();
+		
+		convertToGreyScale(image);
+		Core.bitwise_not(image, result);
+		Imgproc.threshold(result, result, 0, 255, THRESH_BINARY + THRESH_OTSU);
+
+		// Get the deskewed angle and rotate.
+		double angle = getAngle(result);
+		
+		// Do the rotation transformation.
+		Point center = new Point(result.width() / 2, result.height() / 2);
+		Mat rotImage = Imgproc.getRotationMatrix2D(center, angle, 1.0);
+		Size size = new Size(result.width(), result.height());
+		Imgproc.warpAffine(result, result, rotImage, size, Imgproc.INTER_LINEAR + Imgproc.CV_WARP_FILL_OUTLIERS);
+		
+		//Revert the inversion.
+		Core.bitwise_not(result, result);
+		
+		return result;	
+	}
+
+	
+	/**
+	 * Converts to a grey scale.
+	 * @param matrix the matrix to convert.
+	 * @return the converted matrix.
+	 */
+	public Mat convertToGreyScale(Mat matrix) {
+		
+		// Only convert to grey is the image has more than 1 channel.
+		if (matrix.channels() > 1) {
+			Imgproc.cvtColor(matrix, matrix, COLOR_BGR2GRAY);
+		} 
+		
+		return matrix;
+	}
+	
+	/**
+	 * Helper method to determine the angle of skewness in a picture.
+	 * @param matrix	 the matrix to check. 
+	 * @return the angle calculated.
+	 */
+	protected double getAngle(Mat matrix) {
+		
+		Size size = matrix.size();
+		Mat lines = new Mat();
+		Imgproc.HoughLinesP(matrix, lines, 1, Math.PI / 180, 100, size.width / 2.f, 20);
+		double angle = 0.;
+		for (int i = 0; i < lines.height(); i++) {
+			for (int j = 0; j < lines.width(); j++) {
+				angle += Math.atan2(lines.get(i, j)[3] - lines.get(i, j)[1], lines.get(i, j)[2] - lines.get(i, j)[0]);
+			}
+		}
+		angle /= lines.size().area();
+		angle = angle * 180 / Math.PI;
+		return angle;
+	}
+
+	// public InputStream deskew(InputStream fileInputStream) throws Exception {
+	//
+	// BufferedImage bi = ImageIO.read(fileInputStream);
+	// ImageDeskew id = new ImageDeskew(bi);
+	// double imageSkewAngle = id.getSkewAngle();
+	// double MINIMUM_DESKEW_THRESHOLD = 0.05d;
+	//
+	// if ((imageSkewAngle > MINIMUM_DESKEW_THRESHOLD || imageSkewAngle <
+	// -(MINIMUM_DESKEW_THRESHOLD))) {
+	// bi = ImageHelper.rotateImage(bi, -imageSkewAngle);
+	// }
+	//
+	// return IOHelper.bufferedImageToInputStream(bi);
+	// }
+
+	// public InputStream toGreyScale(InputStream fileInputStream) throws Exception
+	// {
+	// BufferedImage bi = ImageIO.read(fileInputStream);
+	// BufferedImage bibw = ImageHelper.convertImageToGrayscale(bi);
+	// return IOHelper.bufferedImageToInputStream(bibw);
+	// }
+
+	// public InputStream binarize(InputStream fileInputStream) throws Exception {
+	// OpenCV.loadShared();
+	// byte[] bytes = IOUtils.toByteArray(fileInputStream);
+	// Mat gray = Imgcodecs.imdecode(new MatOfByte(bytes), CV_LOAD_IMAGE_GRAYSCALE);
+	// Mat gray_with_gauss = new Mat();
+	// Mat finalResult = new Mat();
+	//
+	// GaussianBlur(gray, gray_with_gauss, new Size(5, 5), 0);
+	// Imgproc.threshold(gray_with_gauss, finalResult, 0, 255, THRESH_BINARY +
+	// THRESH_OTSU);
+	//
+	// return IOHelper.Mat2InputStream(finalResult);
+	// }
 }
