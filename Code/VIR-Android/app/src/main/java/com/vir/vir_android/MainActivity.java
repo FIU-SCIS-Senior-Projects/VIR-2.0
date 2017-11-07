918ecfa9917d727882c680f5e9c157c101c67e6d
Alfredo Lopez <alfredlopezf@hotmail.com>
2017-11-07 17:50:08 -0500

    Removed all the versions of Android that are not needed

--- Code/VIR-Android/app/src/main/java/com/vir/vir_android/MainActivity.java
+++ Code/VIR-Android/app/src/main/java/com/vir/vir_android/MainActivity.java
@@ -38,125 +38,80 @@ public class MainActivity extends AppCompatActivity {
     private String mCM;
     private ValueCallback<Uri> mUM;
     private ValueCallback<Uri[]> mUMA;
-    private final static int FCR=1;
+    private final static int FCR = 1;
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
-        if(Build.VERSION.SDK_INT >= 21){
-            Uri[] results = null;
-            //Check if response is positive
-            if(resultCode== Activity.RESULT_OK){
-                if(requestCode == FCR){
-                    if(null == mUMA){
-                        return;
+
+        Uri[] results = null;
+        //Check if response is positive
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == FCR) {
+                if (null == mUMA) {
+                    return;
+                }
+                if (intent == null || intent.getData() == null) {
+                    //Capture Photo if no image available
+                    if (mCM != null) {
+                        results = new Uri[]{Uri.parse(mCM)};
                     }
-                    if(intent == null || intent.getData() == null){
-                        //Capture Photo if no image available
-                        if(mCM != null){
-                            results = new Uri[]{Uri.parse(mCM)};
-                        }
-                    }else{
-                        String dataString = intent.getDataString();
-                        if(dataString != null){
-                            results = new Uri[]{Uri.parse(dataString)};
-                        }
+                } else {
+                    String dataString = intent.getDataString();
+                    if (dataString != null) {
+                        results = new Uri[]{Uri.parse(dataString)};
                     }
                 }
             }
-            mUMA.onReceiveValue(results);
-            mUMA = null;
-        }else{
-            if(requestCode == FCR){
-                if(null == mUM) return;
-                Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
-                mUM.onReceiveValue(result);
-                mUM = null;
-            }
         }
+        mUMA.onReceiveValue(results);
+        mUMA = null;
     }
 
     @SuppressLint({"SetJavaScriptEnabled", "WrongViewCast"})
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if(Build.VERSION.SDK_INT >=23 && (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)) {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1);
-        }
 
         webView = (WebView) findViewById(R.id.activity_main_webview);
         assert webView != null;
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setAllowFileAccess(true);
+        webSettings.setMixedContentMode(0);
+        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
 
-        if(Build.VERSION.SDK_INT >= 21){
-            webSettings.setMixedContentMode(0);
-            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
-        }else if(Build.VERSION.SDK_INT >= 19){
-            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
-        }
         webView.setWebViewClient(new Callback());
         webView.loadUrl(DOMAIN_FULL);
 
-        webView.setWebChromeClient(new WebChromeClient(){
-            final String[] mimeTypes =  {
-                        "application/msword",
-                        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-                        "application/pdf",
-                        "image/*"};
+        webView.setWebChromeClient(new WebChromeClient() {
+            final String[] mimeTypes = {
+                    "application/msword",
+                    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                    "application/pdf",
+                    "image/*"};
 
-            //For Android 3.0+
-            public void openFileChooser(ValueCallback<Uri> uploadMsg){
-                mUM = uploadMsg;
-                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-                i.addCategory(Intent.CATEGORY_OPENABLE);
-                i.setType("*/*");
-                i.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
-                MainActivity.this.startActivityForResult(Intent.createChooser(i,"File Chooser"), FCR);
-            }
-            // For Android 3.0+, above method not supported in some android 3+ versions, in such case we use this
-            public void openFileChooser(ValueCallback uploadMsg, String acceptType){
-                mUM = uploadMsg;
-                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-                i.addCategory(Intent.CATEGORY_OPENABLE);
-                i.setType("*/*");
-                MainActivity.this.startActivityForResult(
-                        Intent.createChooser(i, "File Browser"),
-                        FCR);
-            }
-            //For Android 4.1+
-            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture){
-                mUM = uploadMsg;
-                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-                i.addCategory(Intent.CATEGORY_OPENABLE);
-                i.setType("*/*");
-                i.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
-                MainActivity.this.startActivityForResult(Intent.createChooser(i, "File Chooser"), MainActivity.FCR);
-            }
-            //For Android 5.0+
             public boolean onShowFileChooser(
                     WebView webView, ValueCallback<Uri[]> filePathCallback,
-                    WebChromeClient.FileChooserParams fileChooserParams){
-                if(mUMA != null){
+                    WebChromeClient.FileChooserParams fileChooserParams) {
+                if (mUMA != null) {
                     mUMA.onReceiveValue(null);
                 }
                 mUMA = filePathCallback;
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if(takePictureIntent.resolveActivity(MainActivity.this.getPackageManager()) != null){
+                if (takePictureIntent.resolveActivity(MainActivity.this.getPackageManager()) != null) {
                     File photoFile = null;
-                    try{
+                    try {
                         photoFile = createImageFile();
                         takePictureIntent.putExtra("PhotoPath", mCM);
-                    }catch(IOException ex){
+                    } catch (IOException ex) {
                         Log.e(TAG, "Image file creation failed", ex);
                     }
-                    if(photoFile != null){
+                    if (photoFile != null) {
                         mCM = "file:" + photoFile.getAbsolutePath();
                         takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
-                    }else{
+                    } else {
                         takePictureIntent = null;
                     }
                 }
@@ -166,9 +121,9 @@ public class MainActivity extends AppCompatActivity {
                 contentSelectionIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
 
                 Intent[] intentArray;
-                if(takePictureIntent != null){
+                if (takePictureIntent != null) {
                     intentArray = new Intent[]{takePictureIntent};
-                }else{
+                } else {
                     intentArray = new Intent[0];
                 }
 
@@ -181,26 +136,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    public class Callback extends WebViewClient{
-        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl){
+
+    public class Callback extends WebViewClient {
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
             Toast.makeText(getApplicationContext(), "Failed loading app!", Toast.LENGTH_SHORT).show();
         }
     }
+
     // Create an image file
-    private File createImageFile() throws IOException{
+    private File createImageFile() throws IOException {
         @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "img_"+timeStamp+"_";
+        String imageFileName = "img_" + timeStamp + "_";
         File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        return File.createTempFile(imageFileName,".jpg",storageDir);
+        return File.createTempFile(imageFileName, ".jpg", storageDir);
     }
+
     @Override
-    public boolean onKeyDown(int keyCode, @NonNull KeyEvent event){
-        if(event.getAction() == KeyEvent.ACTION_DOWN){
-            switch(keyCode){
+    public boolean onKeyDown(int keyCode, @NonNull KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
                 case KeyEvent.KEYCODE_BACK:
-                    if(webView.canGoBack()){
+                    if (webView.canGoBack()) {
                         webView.goBack();
-                    }else{
+                    } else {
                         finish();
                     }
                     return true;
