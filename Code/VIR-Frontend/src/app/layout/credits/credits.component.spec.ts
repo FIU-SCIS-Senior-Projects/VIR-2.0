af5f1c8f279f83e10f91a9beaca4a9749953537e
Alfredo Lopez <alfredlopezf@hotmail.com>
2017-11-19 15:18:39 -0500

    Added a few more tests

--- Code/VIR-Frontend/src/app/layout/credits/credits.component.spec.ts
+++ Code/VIR-Frontend/src/app/layout/credits/credits.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
 import { CreditsComponent } from './credits.component';
 
 describe('CreditsComponent', () => {
   let component: CreditsComponent;
   let fixture: ComponentFixture<CreditsComponent>;
+  let de:      DebugElement;
+  let el:      HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +19,18 @@ describe('CreditsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CreditsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    //fixture.detectChanges();
+
+    // query for the title <h1> by CSS element selector
+    de = fixture.debugElement.query(By.css('div'));
+    el = de.nativeElement;
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain the references', () => {
+      expect(el.textContent).toContain('References');
   });
 });
